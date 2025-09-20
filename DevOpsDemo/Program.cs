7275b8dc6f8e5c517b1a2b0b6f0c4194d9359237
Akshay Raut <17160228+AkshayRaut1321@users.noreply.github.com>
2025-09-21 04:20:01 +0530

    Fixed - controller file endpoints not showing up in swagger

--- DevOpsDemo/Program.cs
+++ DevOpsDemo/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -26,6 +27,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// ✅ Map controller routes
+app.MapControllers();
 
 var summaries = new[]
 {
