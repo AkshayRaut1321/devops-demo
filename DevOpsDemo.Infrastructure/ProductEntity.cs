b9b1f5238bd4d2080c27bd0bf1141205ff308308
Akshay Raut <17160228+AkshayRaut1321@users.noreply.github.com>
2025-09-21 01:04:28 +0530

    Added ProductEntity class and MongoDB nuget in Infrastructure

---
+++ DevOpsDemo.Infrastructure/ProductEntity.cs
@@ -0,0 +1,24 @@
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Product
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+    [BsonElement("Name")]
+    public string Name { get; set; } = null!;
+
+    [BsonElement("Description")]
+    public string Description { get; set; } = null!;
+
+    [BsonElement("Category")]
+    public string Category { get; set; } = null!;
+
+    [BsonElement("Price")]
+    public decimal Price { get; set; }
+
+    [BsonElement("CreatedAt")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
