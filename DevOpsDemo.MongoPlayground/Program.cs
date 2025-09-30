13782ce36d492278c74a41a84ab2e14e2b2c77fc
Akshay Raut <17160228+AkshayRaut1321@users.noreply.github.com>
2025-09-30 18:18:50 +0530

    Playground project is ready and runnable based on Debugger option

---
+++ DevOpsDemo.MongoPlayground/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Events;
+
+Console.WriteLine("Hello, World!");
+
+var mongoSettings = MongoClientSettings.FromConnectionString("mongodb://localhost:8001");
+
+mongoSettings.ClusterConfigurator = cb =>
+{
+    cb.Subscribe<CommandStartedEvent>(e =>
+    {
+        Console.WriteLine($"Mongo Command Started: {e.CommandName} - {e.Command.ToJson()}");
+    });
+};
+
+var client = new MongoClient(mongoSettings);
+var db = client.GetDatabase("Playground");
+
+// Pick the demo you want to run
+await Playground.BasicCrud.RunAsync(db);
+// await Playground.PaginationPlayground.RunAsync(db);
