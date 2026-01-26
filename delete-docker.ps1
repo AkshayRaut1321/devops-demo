0dedfc0f4d8defdbdd2c6675d2998775ad44ff17
Akshay Raut <17160228+AkshayRaut1321@users.noreply.github.com>
2026-01-26 21:17:01 +0530

    Automated initializing replica set for Mongo using mongo-init.js also auto-removal of volumes so next time replica initlaization doesn't error also

--- delete-docker.ps1
+++ delete-docker.ps1
@@ -1,6 +1,8 @@
+Write-Host "Stopping containers and deleting volumes..."
+
 docker compose `
   -f docker-compose.dotnetapi.yml `
   -f docker-compose.mongodb.yml `
   -f docker-compose.elasticsearch.yml `
   -f docker-compose.dotnetindexworker.yml `
-  down
+  down -v --remove-orphans
